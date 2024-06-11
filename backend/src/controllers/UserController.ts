ea70bb716bc4b302006939dc110aa5db171ba36e
luizgustavojunqueira <luizgustavossj@gmail.com>
2024-06-11 08:22:31 -0400

    add docker compose

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,85 +1,92 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import { UserRequest } from "../interfaces/UserRequest";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jsonwebtoken from "jsonwebtoken";
 
 export class Controller {
-	async register(req: Request, res: Response) {
-		try {
-			const prisma = new PrismaClient();
+  async register(req: Request, res: Response) {
+    try {
+      console.log("UAU");
+      const prisma = new PrismaClient();
 
-			const newUser = req.body;
 
-			const user = await prisma.user.create({
-				data: { ...newUser, password: await bcrypt.hash(newUser.password, 10) },
-			});
+      const newUser = req.body;
 
-			return res.json(user);
-		} catch (error) {
-			return res.status(500).json({ message: error });
-		}
-	}
+      console.log("teste");
 
-	async login(req: Request, res: Response) {
-		const prisma = new PrismaClient();
+      console.log(newUser);
 
-		const { email, password } = req.body;
+      const user = await prisma.user.create({
+        data: { ...newUser, password: await bcrypt.hash(newUser.password, 10) },
+      });
 
-		const user = await prisma.user.findUnique({
-			where: { email: email },
-		});
+      console.log("UAU2");
+      return res.json(user);
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  }
 
-		if (user == null) {
-			res.status(404).json({ message: "User not found!" });
-		} else if (await bcrypt.compare(password, user.password)) {
-			const jwt = jsonwebtoken.sign(
-				{ user: { ...user, password: undefined } },
-				process.env.JWT_SECRET as string,
-				{ expiresIn: "15m" }
-			);
+  async login(req: Request, res: Response) {
+    const prisma = new PrismaClient();
 
-			res.cookie("token", jwt);
-			res.json({ message: "Logged in!" });
-		} else {
-			res.status(401).send("Invalid credentials!");
-		}
-	}
+    const { email, password } = req.body;
 
-	async deleteUser(req: UserRequest, res: Response) {
-		try {
-			const prisma = new PrismaClient();
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
 
-			const email = req.headers.user?.email;
+    if (user == null) {
+      res.status(404).json({ message: "User not found!" });
+    } else if (await bcrypt.compare(password, user.password)) {
+      const jwt = jsonwebtoken.sign(
+        { user: { ...user, password: undefined } },
+        process.env.JWT_SECRET as string,
+        { expiresIn: "15m" }
+      );
 
-			if (email) {
-				const events = await prisma.event.deleteMany({
-					where: { userEmail: email },
-				});
+      res.cookie("token", jwt);
+      res.json({ message: "Logged in!" });
+    } else {
+      res.status(401).send("Invalid credentials!");
+    }
+  }
 
-				const user = await prisma.user.delete({
-					where: { email: email },
-				});
+  async deleteUser(req: UserRequest, res: Response) {
+    try {
+      const prisma = new PrismaClient();
 
-				res.cookie("token", "");
-				return res.json({ user: {...user, password: undefined}, events: events });
-			}
-		} catch (error) {
-			return res.status(500).json({ message: error });
-		}
-	}
+      const email = req.headers.user?.email;
 
-	async getUsers(req: Request, res: Response) {
-		const prisma = new PrismaClient();
+      if (email) {
+        const events = await prisma.event.deleteMany({
+          where: { userEmail: email },
+        });
 
-		const users = await prisma.user.findMany();
+        const user = await prisma.user.delete({
+          where: { email: email },
+        });
 
-		const usersWithoutPassword = users.map((user) => {
-			return { ...user, password: undefined };
-		});
+        res.cookie("token", "");
+        return res.json({ user: { ...user, password: undefined }, events: events });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  }
 
-		return res.json(usersWithoutPassword);
-	}
+  async getUsers(req: Request, res: Response) {
+    const prisma = new PrismaClient();
+
+    const users = await prisma.user.findMany();
+
+    const usersWithoutPassword = users.map((user) => {
+      return { ...user, password: undefined };
+    });
+
+    return res.json(usersWithoutPassword);
+  }
 }
 
 export const UserController = new Controller();
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -9,6 +9,6 @@ router.post("/register", registerValidation, UserController.register);
 router.delete("/user", validateToken, UserController.deleteUser);
 router.post("/login", UserController.login);
 router.get("/users", validateToken, UserController.getUsers);
-router.get("/test", validateToken, (req, res) => {res.json({message: "valid token"})});
+router.get("/test", validateToken, (req, res) => { res.json({ message: "valid token" }) });
 
 export default router;
