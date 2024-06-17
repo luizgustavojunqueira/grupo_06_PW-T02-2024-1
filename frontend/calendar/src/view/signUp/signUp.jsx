52752de0824e8d19facbe035f58a97de847b7bba
luizgustavojunqueira <luizgustavossj@gmail.com>
2024-06-16 23:19:25 -0400

    fix input register view

--- frontend/calendar/src/view/signUp/signUp.jsx
+++ frontend/calendar/src/view/signUp/signUp.jsx
@@ -6,60 +6,59 @@ import iconPassword from "../../assets/utils/img/iconPassword.png"
 import './signUp.css'
 
 
-
-
 export default function SignUp() {
   const [name, setName] = useState("");
-  const [email, setEmail] = useState ("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
 
-  
+
   async function handleSignUp(e) {
     e.preventDefault();
-    
-    if(name && email && password){
-        console.log(name, email, password);
+
+    if (name && email && password) {
+      console.log(name, email, password);
     }
 
 
-    
+
   }
 
   return (
 
-    
-    <div className="mainContainer"> 
-    
-    <section className="sideSection">
+
+    <div className="mainContainer">
+
+
+      <section className="sideSection">
         <div className="container">
-            <h1>Bem-vindo de volta!</h1>
-            <p>acesse sua conta agora</p>
-            <a href="./login.html">
-                <button>Login</button>
-            </a>
+          <h1>Bem-vindo de volta!</h1>
+          <p>acesse sua conta agora</p>
+          <a href="./login.html">
+            <button>Login</button>
+          </a>
         </div>
-    </section>
-    <main className="mainContent">
+      </section>
+      <main className="mainContent">
         <h1>Crie sua conta</h1>
         <form className="formSignUp" onSubmit={handleSignUp}>
-            <div className="inputAndIcon">
-                <img src={iconName}alt=""/>
-                <input className="input" type="email" id="name" value = {name} onChange = {(e) => {setName(e.target.value)}} placeholder="Nome"/>
-            </div>
-            <div className="inputAndIcon">
-                <img src={iconEmail} alt=""/>
-                <input className="input" type="email" id="email"  value ={email} onChange={(e) => {setEmail(e.target.value)}} placeholder="Email"/>
-            </div>
-            <div className="inputAndIcon">
-                <img src={iconPassword} alt=""/>
-                <input className="input" type="password" id="password" value = {password} onChange = {(e) => {setPassword(e.target.value)}} placeholder="Senha"/>
-            </div>
-            <input className="buttonInput" type="submit" value="Cadastrar"/>
+          <div className="inputAndIcon">
+            <img src={iconName} alt="" />
+            <input className="input" type="email" id="name" value={name} onChange={(e) => { setName(e.target.value) }} placeholder="Nome" />
+          </div>
+          <div className="inputAndIcon">
+            <img src={iconEmail} alt="" />
+            <input className="input" type="email" id="email" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder="Email" />
+          </div>
+          <div className="inputAndIcon">
+            <img src={iconPassword} alt="" />
+            <input className="input" type="password" id="password" value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder="Senha" />
+          </div>
+          <input className="buttonInput" type="submit" value="Cadastrar" />
         </form>
-    </main>
+      </main>
 
-</div>
+    </div>
 
   );
 }
