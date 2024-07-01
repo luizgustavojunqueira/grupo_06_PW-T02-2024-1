a03924c1fd5b80e8e6b512523fddafc7dbfc7f3e
Luiz Gustavo <luizgustavossj@gmail.com>
2024-07-01 13:38:15 -0400

    remove rota desnecessária

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -9,6 +9,5 @@ router.post("/register", registerValidation, UserController.register);
 router.delete("/user", validateToken, UserController.deleteUser);
 router.post("/login", UserController.login);
 router.get("/users", validateToken, UserController.getUsers);
-router.get("/test", validateToken, (req, res) => { res.json({ message: "valid token" }) });
 
 export default router;
